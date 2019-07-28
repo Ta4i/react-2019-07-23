aabe3ac602891c93de21d38b8f6203a3fed517c1
Mirzekhan Gamzatov <mirzekhan@gmail.com>
2019-07-28 19:53:58 +0300

    HT1

--- src/components/restaurant-reviews.js
+++ src/components/restaurant-reviews.js
@@ -1,24 +1,28 @@
-import React, {useState} from 'react'
-import {useToggle} from '../custom-hooks/use-toggle'
+import React, { useState } from "react";
+import { useToggle } from "../custom-hooks/use-toggle";
 
 function RestaurantReviews(props) {
-  const [isOpen, toggleOpen] = useToggle()
-  const {restaurant} = props
+  const [isOpen, toggleOpen] = useToggle();
+  const { reviews } = props.restaurant;
 
   return (
     <div>
-      <button
-        onClick={toggleOpen}
-      >{
-        isOpen ? 'Hide reviews' : 'Show reviews'
-      }</button>
-      {
-        isOpen ?
-          (<>Reviews: {restaurant.reviews.length}</>) :
-          null
-      }
+      <button onClick={toggleOpen}>
+        {isOpen ? "Hide reviews" : "Show reviews"}
+      </button>
+      {isOpen ? (
+        <ul>
+          {reviews.map(review => (
+            <li key={review.id}>
+              <h3>{review.user}</h3>
+
+              <p>{review.text}</p>
+            </li>
+          ))}
+        </ul>
+      ) : null}
     </div>
-  )
+  );
 }
 
-export default RestaurantReviews
+export default RestaurantReviews;
