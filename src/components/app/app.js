b24f09b573fa486eb85720409e17d7e6dea5849f
Maksym Kramarenko <maksym@wix.com>
2019-07-30 21:07:50 +0300

    Add form

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,7 @@ import { Layout } from "antd"
 import './app.css';
 import Header from '../header'
 import RestaurantList from "../restaurant-list"
+import Order from '../order'
 // import RestaurantsMap from '../restaurants-map'
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
         <main role="main">
           <RestaurantList restaurants={this.props.restaurants} />
           {/*<RestaurantsMap restaurants={this.props.restaurants} />*/}
+          <Order />
         </main>
       </Layout>
     );
