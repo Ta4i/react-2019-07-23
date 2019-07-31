da6ab581b7801fface58de8d54f3d8b102346bfe
tinyakovru <a@tinyakov.ru>
2019-07-31 23:28:29 +0300

    sync

---
+++ src/components/restaurant-list/restaurants-list.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { List } from "antd";
+import Restaurant from "../restaurant";
+import { accordion } from "../../decorators/accordion";
+
+class RestaurantList extends Component {
+  render() {
+    const {
+      restaurants,
+
+      // from decorator
+      openItemId,
+      toggleOpen
+    } = this.props;
+    return (
+      <List
+        itemLayout={"horizontal"}
+        dataSource={restaurants}
+        renderItem={restaurant => (
+          <Restaurant
+            key={restaurant.id}
+            restaurant={restaurant}
+            isMenuOpen={openItemId === restaurant.id}
+            toggleOpenMenu={toggleOpen}
+          />
+        )}
+      />
+    );
+  }
+}
+
+export default accordion(RestaurantList);
---
+++ src/components/restaurant-menu/retaurant-menu.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Dish from "../dish";
+import { Row, Col } from "antd";
+
+function RestaurantMenu(props) {
+  return (
+    <div
+      style={{ padding: "16px" }}
+      data-autoid={`MENU_ITEMS_${props.restaurantId}`}
+    >
+      <Row gutter={16}>
+        {props.menu.map(dish => (
+          <Col key={dish.id} span={8}>
+            <Dish {...dish} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
+
+export default RestaurantMenu;
