53d4e2e5c352f86f0ca6b4c712615e2b81265335
Шмидт Сергей <sshmidt@ad.carcade.com>
2019-08-01 21:05:42 +0300

    Add restaurant review test

--- src/components/restaurant-reviews/restaurant-reviews.test.js
+++ src/components/restaurant-reviews/restaurant-reviews.test.js
@@ -1,14 +1,29 @@
 import React from "react";
 import Enzyme, { mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import RestaurantReviews from "./restaurant-reviews";
 import { restaurants } from "../../fixtures";
-import Item from "antd/lib/list/Item";
-import { exportAllDeclaration } from "@babel/types";
+import { Comment, Rate, List } from "antd";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("At starts Restaurant-reviews test", function() {
-  it("It should work", function() {
-    expect(true).toEqual(false);
+  it("should show all reviews", function() {
+    const wrapper = mount(<RestaurantReviews restaurant={restaurants[0]} />);
+    expect(wrapper.find('div[data-autoid="REVIEW_ITEM"]').length).toEqual(2);
+  });
+});
+
+describe("Show Rate", function() {
+  it("should show all reviews", function() {
+    const wrapper = mount(<RestaurantReviews restaurant={restaurants[0]} />);
+    expect(wrapper.find(Rate).length).toEqual(2);
+  });
+});
+
+describe("Count of full star", function() {
+  it("should show 8 stars", function() {
+    const wrapper = mount(<RestaurantReviews restaurant={restaurants[0]} />);
+    expect(wrapper.find(".ant-rate-star-full").length).toEqual(8);
   });
 });
