c11a52b4154359f571b823180dc9e5622c3ce827
AlexanderMarginal <alexander.marginal@gmail.com>
2019-08-02 21:30:36 +0300

    ht3 - added clear dish button

--- src/store/reducer/cart.js
+++ src/store/reducer/cart.js
@@ -1,26 +1,42 @@
 export default (cartState = {}, action) => {
   switch (action.type) {
-    case 'ADD_DISH': {
-      const {id} = action.payload
+    case "ADD_DISH": {
+      const { id } = action.payload;
       return {
         ...cartState,
         [id]: cartState[id] ? cartState[id] + 1 : 1
-      }
+      };
     }
-    case 'REMOVE_DISH': {
-      const {id} = action.payload
+    case "REMOVE_DISH": {
+      const { id } = action.payload;
       if (!cartState[id]) {
-        return cartState
+        return cartState;
       }
-      const newCartState = {...cartState}
+      const newCartState = { ...cartState };
       if (newCartState[id] === 1) {
-        delete newCartState[id]
+        delete newCartState[id];
       } else {
-        newCartState[id] = newCartState[id] - 1
+        newCartState[id] = newCartState[id] - 1;
       }
-      return newCartState
+      return newCartState;
+    }
+    case "CLEAR_DISH": {
+      const { id } = action.payload;
+      if (!cartState[id]) {
+        return cartState;
+      }
+
+      const newCartState = { ...cartState };
+
+      if (newCartState[id] === 1) {
+        delete newCartState[id];
+      } else {
+        newCartState[id] = 0;
+      }
+
+      return newCartState;
     }
     default:
-      return cartState
+      return cartState;
   }
-}
+};
