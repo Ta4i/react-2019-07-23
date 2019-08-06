c2f7f1c80a63977127cc922e065d5820c85da168
Maksym Kramarenko <maksym@wix.com>
2019-08-06 20:35:03 +0300

    Add Class component with selectors

--- src/components/dish/dish.js
+++ src/components/dish/dish.js
@@ -1,49 +1,55 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Card, Button} from 'antd'
-import {useDispatch, useSelector} from 'react-redux'
+import {connect} from 'react-redux'
 import {addDishToCart, subtractDishFromCart} from '../../store/ac'
 import Price from '../price'
 import {selectDish, selectDishAmount} from '../../store/selectors'
 
-function Dish(props) {
-  const {id} = props
-  const dispatch = useDispatch()
-  const dish = useSelector(state => selectDish(state, props))
-  const amount = useSelector(state => selectDishAmount(state, props))
+class Dish extends Component {
+  render() {
+    const {id, dish, amount} = this.props
+    const dispatch = this.props.dispatch
 
-  console.log('Dish')
+    console.log('Dish')
 
-  return (
-    <Card
-      bordered
-      actions={[
-        <Price value={dish.price} />,
-        <>
-          <span style={{margin: '0 12px'}} data-autoid={`DISH_AMOUNT_${id}`}>
-            {amount}
-          </span>
-          <Button.Group>
-            <Button
-              onClick={() => dispatch(subtractDishFromCart(id))}
-              type="primary"
-              shape="circle"
-              icon="minus"
-              data-autoid={`REMOVE_DISH_${id}`}
-            />
-            <Button
-              onClick={() => dispatch(addDishToCart(id))}
-              type="primary"
-              shape="circle"
-              icon="plus"
-              data-autoid={`ADD_DISH_${id}`}
-            />
-          </Button.Group>
-        </>,
-      ]}
-    >
-      <Card.Meta title={dish.name} description={dish.ingredients.join(', ')} />
-    </Card>
-  )
+    return (
+      <Card
+        bordered
+        actions={[
+          <Price value={dish.price} />,
+          <>
+            <span style={{margin: '0 12px'}} data-autoid={`DISH_AMOUNT_${id}`}>
+              {amount}
+            </span>
+            <Button.Group>
+              <Button
+                onClick={() => dispatch(subtractDishFromCart(id))}
+                type="primary"
+                shape="circle"
+                icon="minus"
+                data-autoid={`REMOVE_DISH_${id}`}
+              />
+              <Button
+                onClick={() => dispatch(addDishToCart(id))}
+                type="primary"
+                shape="circle"
+                icon="plus"
+                data-autoid={`ADD_DISH_${id}`}
+              />
+            </Button.Group>
+          </>,
+        ]}
+      >
+        <Card.Meta
+          title={dish.name}
+          description={dish.ingredients.join(', ')}
+        />
+      </Card>
+    )
+  }
 }
 
-export default Dish
+export default connect((state, ownProps) => ({
+  dish: selectDish(state, ownProps),
+  amount: selectDishAmount(state, ownProps),
+}))(Dish)
