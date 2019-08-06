0899c033049653560745cc2187b7679271064225
Maksym Kramarenko <maksym@wix.com>
2019-08-06 21:00:03 +0300

    Add Redux Devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import {createStore, applyMiddleware} from 'redux'
 import reducer from './reducer'
 import logger from './middlewares/logger'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-const enhancer = applyMiddleware(logger)
+const composeEnhancer = composeWithDevTools({})
+const enhancer = composeEnhancer(applyMiddleware(logger))
 
 const store = createStore(reducer, enhancer)
 
