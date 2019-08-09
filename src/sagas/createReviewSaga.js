99b2c9181a028e8e3ec144007f6a82c01e4492c0
Alex Sam <alexgui@yandex.ru>
2019-08-09 16:53:23 +0300

    some improvements

--- src/sagas/createReviewSaga.js
+++ src/sagas/createReviewSaga.js
@@ -1,5 +1,5 @@
 import {put, takeEvery, select} from 'redux-saga/effects'
-import {head, isEmpty} from 'lodash'
+import {head, isEmpty, values} from 'lodash'
 import {SUBMIT_REVIEW_FORM} from '../store/constants'
 import {
   successReviewForm,
@@ -17,8 +17,8 @@ export function* createReviewSaga({payload}) {
   const {rate, reviewText, restaurant, name} = payload
 
   try {
-    const existUser = Object.values(users).filter(user => user.name === name)
-    const user = isEmpty(existUser) ? {id: uuidv1(), name} : head(existUser)
+    const existUser = values(users).filter(user => user.name === name)
+    const user = head(existUser) || {id: uuidv1(), name}
 
     if (isEmpty(existUser)) {
       yield put(addNewUser(user))
@@ -27,7 +27,7 @@ export function* createReviewSaga({payload}) {
     const review = new ReviewModel({
       restaurantId: restaurant,
       userId: user.id,
-      reviewText: reviewText || '',
+      reviewText: reviewText,
       rate: rate,
     })
 
